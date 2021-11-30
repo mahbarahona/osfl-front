06545609f45e5185b4c0de9729a8bceec58d0d65
Manuel Hernandez <mahbarahona@gmail.com>
2021-11-30 13:38:58 -0400

    se hace focus en input nombre de organizacion en landing page

--- src/app/balance/pages/landing-page/landing-page.component.ts
+++ src/app/balance/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef,  ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { NavigationService } from 'src/app/core/navigation/navigation.service';
 import { BalanceStoreService } from '../../store/balance.store';
@@ -8,8 +8,9 @@ import { BalanceStoreService } from '../../store/balance.store';
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements AfterViewInit {
 
+  @ViewChild('nombre') nombre:ElementRef = new ElementRef('')
   nombre_organizacion = new FormControl('',[Validators.required])
 
 
@@ -18,8 +19,10 @@ export class LandingPageComponent implements OnInit {
     public store:BalanceStoreService,
     private nav:NavigationService) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.nombre.nativeElement.focus()
   }
+
 
   comenzar(){
     this.nav.ir('/balance/item')
